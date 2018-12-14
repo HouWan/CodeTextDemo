7bff9289f606718733fbc73357ae299267afb690
HouWan <houwan136@foxmail.com>
2018-12-14 15:14:14 +0800

    无奈
    无奈

--- CodeTextDemo/ViewController.h
+++ CodeTextDemo/ViewController.h
@@ -7,6 +7,13 @@
 
 #import <UIKit/UIKit.h>
 
+// 屏幕宽度高度
+#define SW ([UIScreen mainScreen].bounds.size.width)
+#define SH ([UIScreen mainScreen].bounds.size.height)
+
+// 随机色
+#define HWRandomColor [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1.0]
+
 @interface ViewController : UIViewController
 
 
